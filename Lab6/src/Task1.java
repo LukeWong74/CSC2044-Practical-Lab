5b24bf6914c98af72375ec22dfe37bba9490d57a
LukeWong74 <14070262@imail.sunway.edu.my>
2016-05-05 19:12:49 +0800

    Update Task1.java

--- Lab6/src/Task1.java
+++ Lab6/src/Task1.java
@@ -16,39 +16,44 @@ public class Task1 implements Runnable {
 	@Override
 	public void run() {
 		double result = 0;
-		double tempValue = 0;
-		double min = Double.MAX_VALUE;
 
 		// TODO Auto-generated method stub
 		for (int x = 0; x < 350; x++) {
-			
+			result = 0;
 			for (int y = 0; y < 100; y++) {
 				result += (array[y] * array[y]) * y;
 			}
 
+			if (result < minimumValue) {
+				minimumValue = result;
+
+				passArray(tempArray, array);
+			} else {
+				for (int z = 0; z < 100; z++)
+					passArray(array, tempArray);
+			}
+
 			for (int b = 0; b < 100; b++) {
-				double value = -0.5 + rdm.nextDouble() * 0.5;
-				tempValue = array[b];
+				double initialMin = -0.5;
+				double initialMax = 0.5;
+				double value = initialMin + rdm.nextDouble()
+						* (initialMax - initialMin);
 
 				if (array[b] + value > -5.12 && array[b] + value < 5.12)
 					array[b] += value;
-				else
-					array[b] = tempValue;
 			}
 
-			if (result < min) {
-				min = result;
-
-				if (min < minimumValue)
-					minimumValue = min;
-
-				for (int z = 0; z < 100; z++)
-					tempArray[z] = array[z];
-			} else {
-				for (int z = 0; z < 100; z++)
-					array[z] = tempArray[z];
+			if (x % 20 == 0) {
+				System.out.printf("Thread: %d - Current Minimum Value: %f\n",
+						length, minimumValue);
 			}
 		}
+
 		Main.minArray[length] = minimumValue;
+	}
+
+	public void passArray(double[] firstArray, double[] secondArray) {
+		for (int z = 0; z < 100; z++)
+			firstArray[z] = secondArray[z];
 	}
 }
